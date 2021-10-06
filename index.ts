af734855e37f087629d7ee66f84e38aad939d09d
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-10-06 22:54:48 +0300

    first commit
    Co-Authored-By: Connor <cimok2000@gmail.com>
    Co-Authored-By: Niko Huuskonen <59277828+N1kO23@users.noreply.github.com>
    Co-Authored-By: ChasJC23 <45319866+ChasJC23@users.noreply.github.com>

---
+++ index.ts
@@ -0,0 +1,21 @@
+namespace TheBoys {
+
+  class CHAD extends HOMIE {
+    constructor() {
+      super();
+    }
+  }
+  
+  class NIKO extends CHAD🕵️‍♀️ {
+    constructor() {
+      super();
+      this.penisSize = "100m";
+    }
+  }
+
+  class HOMIE extends PERSON {
+    constructor() {
+      super();
+    }
+  }
+}
