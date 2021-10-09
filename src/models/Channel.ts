a41f0982d2e29bcb2ce1808fe4cf265b279e9808
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-10-09 08:11:00 +0300

    added channels
    Co-Authored-By: Connor <cimok2000@gmail.com>

---
+++ src/models/Channel.ts
@@ -0,0 +1,24 @@
+import mongoose, { Schema } from "mongoose";
+import { settings } from "../settings";
+mongoose.connect(settings.database_url);
+
+export type ChannelTypes = "voice" | "text";
+
+export interface IChannel {
+  name: string;
+  _id: string;
+  type: ChannelTypes;
+}
+
+const schema = new Schema<IChannel>(
+  {
+    _id: { type: String, required: true},
+    name: { type: String, required: true},
+    type: { type: String, required: true},
+  },
+  {
+    versionKey: false,
+  }
+);
+
+export const Channel = mongoose.model<IChannel>("Channel", schema);
--- src/routes/v1/guild.ts
+++ src/routes/v1/guild.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { Member } from "../../models/Member";
 import { v4 as uuidv4 } from "uuid";
+import { Channel } from "../../models/Channel";
 
 const router = express.Router();
 
@@ -10,8 +11,23 @@ router.put("/member/:uuid", async (req, res) => {
   return res.status(201).json(member);
 });
 
+router.put("/channel", async (req, res) => {
+  const {type, name} = req.body;
+  const channel = new Channel({
+    _id: uuidv4(), 
+    name: name.trim(),
+    type: type.trim(),
+  });
+  await channel.save();
+  return res.status(201).json(channel);
+});
+
 router.get("/members", async (req, res) => {
   return res.status(200).json((await Member.find()).map(member => member.member_id));
+});
+
+router.get("/channels", async (req, res) => {
+  return res.status(200).json((await Channel.find()));
 });
 
 export default router;
