fd3046fa2d98eaf69a195454bc32082f93baf216
George Tsotsos <34042825+Geoxor@users.noreply.github.com>
2021-11-08 23:10:42 +0200

    patched potential vulnerability
    Co-Authored-By: Niko Huuskonen <59277828+N1kO23@users.noreply.github.com>
    Co-Authored-By: Connor <cimok2000@gmail.com>

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ import cors from "cors";
 import io from "socket.io";
 import chalk from "chalk";
 import axios from "axios";
-import sharp from "sharp"
+import sharp from "sharp";
 import jwt from "jsonwebtoken";
 import fs from "fs";
 import { Member } from "./models/Member";
@@ -36,13 +36,12 @@ class Server {
   private uuid: string | undefined;
   constructor() {
     console.log(`connecting to mongo ${settings.database_url}`);
-    mongoose.connect(settings.database_url).then(() => console.log('connected to mongo'));
+    mongoose.connect(settings.database_url).then(() => console.log("connected to mongo"));
     this.express = express();
     this.registerExpressRoutes();
     this.server = http.createServer(this.express);
     this.io = new io.Server(this.server, { cors: { origin: "*" } });
     this.io.on("connection", async (socket) => {
-
       console.log("new connection");
       if (!this.token || !socket.handshake.auth) return;
       let userUUID = "";
@@ -56,8 +55,8 @@ class Server {
       }
 
       socket.on("pong", (pong) => {
-        const {cpu, ram, network} = this.stats.dataAll;
-        socket.emit("ping", {pong, cpu, ram, network});
+        const { cpu, ram, network } = this.stats.dataAll;
+        socket.emit("ping", { pong, cpu, ram, network });
       });
 
       socket.on("message", async (data) => {
@@ -97,24 +96,24 @@ class Server {
     this.io.emit(wsEvent, targetPath);
   }
 
-  public async watchExplorer(){
-    if (process.platform !== 'linux') {
-      fs.watch(settings.explorer_directory, {recursive: true}, async (eventType, filePath) => {
-        const absolutePath = settings.explorer_directory + '/' + filePath;
+  public async watchExplorer() {
+    if (process.platform !== "linux") {
+      fs.watch(settings.explorer_directory, { recursive: true }, async (eventType, filePath) => {
+        const absolutePath = settings.explorer_directory + "/" + filePath;
         const targetPath = `./${filePath.replace(/\\/g, "/")}`;
         switch (eventType) {
-          case 'rename':
+          case "rename":
             if (fs.existsSync(absolutePath)) {
               this.io.emit(`explorer:rename`, targetPath);
             } else {
               this.io.emit(`explorer:unlink`, targetPath);
-            };
+            }
           default:
             break;
         }
-      })
+      });
     } else {
-      console.log('using chokidar');
+      console.log("using chokidar");
 
       const watcher = chokidar.watch(settings.explorer_directory, {
         depth: 99,
@@ -122,10 +121,10 @@ class Server {
       });
 
       watcher
-        .on('addDir', path => this.linuxWatcherHandler(path, 'rename'))
-        .on('add',    path => this.linuxWatcherHandler(path, 'rename'))
-        .on('change', path => this.linuxWatcherHandler(path, 'rename'))
-        .on('unlink', path => this.linuxWatcherHandler(path, 'unlink'));
+        .on("addDir", (path) => this.linuxWatcherHandler(path, "rename"))
+        .on("add", (path) => this.linuxWatcherHandler(path, "rename"))
+        .on("change", (path) => this.linuxWatcherHandler(path, "rename"))
+        .on("unlink", (path) => this.linuxWatcherHandler(path, "unlink"));
     }
   }
 
@@ -140,13 +139,13 @@ class Server {
     this.express.use("/static", staticRouter);
     this.express.use("/v1", V1);
     this.express.get("/", (req, res) => res.status(200).json({ message: "hello cunt" }));
-    this.express.get("/metadata", async (req, res) =>
-      res.status(200).json({
-        _id: this.uuid,
-        name: "Test server",
-        members: (await Member.find()).map((member) => member.member_id),
-      })
-    );
+    // this.express.get("/metadata", async (req, res) =>
+    //   res.status(200).json({
+    //     _id: this.uuid,
+    //     name: "Test server",
+    //     members: (await Member.find()).map((member) => member.member_id),
+    //   })
+    // );
     this.express.post("/login", async (req, res) => {
       if (!this.token) return res.status(403);
       const { uuid } = req.body;
@@ -162,8 +161,8 @@ class Server {
       if (fs.existsSync(avatarGIF)) return res.status(200).sendFile(avatarGIF);
       if (fs.existsSync(avatarPNG)) {
         const image = sharp(avatarPNG);
-        const buffer = await image.resize({width: 64}).webp().toBuffer()
-        return res.writeHead(200, [["Content-Type", 'image/webp']]).end(buffer);
+        const buffer = await image.resize({ width: 64 }).webp().toBuffer();
+        return res.writeHead(200, [["Content-Type", "image/webp"]]).end(buffer);
       }
       return res.status(404).send();
     });
@@ -173,9 +172,9 @@ class Server {
       if (fs.existsSync(bannerGIF)) return res.status(200).sendFile(bannerGIF);
       if (fs.existsSync(bannerPNG)) {
         const image = sharp(bannerPNG);
-        const buffer = await image.resize({height: 720}).webp().toBuffer()
-        return res.writeHead(200, [["Content-Type", 'image/webp']]).end(buffer);
-      };
+        const buffer = await image.resize({ height: 720 }).webp().toBuffer();
+        return res.writeHead(200, [["Content-Type", "image/webp"]]).end(buffer);
+      }
       res.status(404).send();
     });
   }
